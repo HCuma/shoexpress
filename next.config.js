891b31c5fa60657ceec7610cfa66ec85e2c31980
HCuma <h.075247cuma@gmail.com>
2024-12-30 17:07:51 +0300

    Update Next.js configuration for Tailwind

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
